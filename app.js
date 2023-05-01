38f01512bd31377c0ac7d3452c62e44c4fde035c
Abhisekh Gupta <abhisekhgupta405@gmail.com>
2023-05-01 17:27:56 +0530

    Added Socket.io

--- app.js
+++ app.js
@@ -63,7 +63,11 @@ mongoose
   .connect(process.env.MONGODB_URI)
   .then((result) => {
     console.log("HO GAYA BHAISAHAB");
-    app.listen(8080);
+    const server = app.listen(8080);
+    const io = require("./socket").init(server);
+    io.on("connection", (socket) => {
+      console.log("Client connected");
+    });
   })
   .catch((err) => {
     console.log(err);
