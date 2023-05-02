988b52e552e81d9c673916d1ef0a04492b92d01a
Abhisekh Gupta <abhisekhgupta405@gmail.com>
2023-05-02 10:02:49 +0530

    ...

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -16,10 +16,10 @@ module.exports = {
       errors.push({ message: "Password too short!" });
     }
     if (errors.length > 0) {
-        const error = new Error("Invalid input.");
-        error.data = errors;
-        error.code = 422;
-        throw error;
+      const error = new Error("Invalid input.");
+      error.data = errors;
+      error.code = 422;
+      throw error;
     }
     const existingUser = await User.findOne({ email: userInput.email });
     if (existingUser) {
