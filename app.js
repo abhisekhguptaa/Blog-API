7b5efaa8d03a7e3fd5641e0128013ea20c6a09f9
Abhisekh Gupta <abhisekhgupta405@gmail.com>
2023-05-03 22:02:12 +0530

    Updated GraphQL queries

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -9,6 +10,7 @@ const { graphqlHTTP } = require("express-graphql");
 
 const graphqlSchema = require("./graphql/schema");
 const graphqlResolver = require("./graphql/resolvers");
+const auth = require("./middleware/auth");
 
 const app = express();
 
@@ -52,6 +54,24 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(auth);
+
+app.put("/post-image", (req, res, next) => {
+  if (!req.isAuth) {
+    throw new Error("Not Authenticated");
+  }
+  if (!req.file) {
+    return res.status(200).json({ message: "No file provided" });
+  }
+  if (req.body.oldPath) {
+    clearImage(req.body.oldPath);
+  }
+  return res.status(201).json({
+    message: "File stored",
+    filePath: req.file.path.replace(/\\/g, "/"),
+  });
+});
+
 app.use(
   "/graphql",
   graphqlHTTP({
@@ -87,3 +107,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, "..", filePath);
+  fs.unlink(filePath, (err) => console.log(err));
+};
--- middleware/auth.js
+++ middleware/auth.js
@@ -3,23 +3,22 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   const authHeader = req.get("Authorization");
   if (!authHeader) {
-    const error = new Error("Not authenticated.");
-    error.statusCode = 401;
-    throw error;
+    req.isAuth = false;
+    return next(); // next() is used to continue to the next middleware
   }
   const token = authHeader.split(" ")[1];
   let decodedToken;
   try {
     decodedToken = jwt.verify(token, "somesupersecretsecret");
   } catch (err) {
-    err.statusCode = 500;
-    throw err;
+    req.isAuth = false;
+    return next(); // next() is used to continue to the next middleware
   }
   if (!decodedToken) {
-    const error = new Error("Not authenticated.");
-    error.statusCode = 401;
-    throw error;
+    req.isAuth = false;
+    return next(); // next() is used to continue to the next middleware
   }
   req.userId = decodedToken.userId;
+  req.isAuth = true;
   next();
 };
